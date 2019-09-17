30ca74d7b898614ed6a01ac0d847231426f6b930
Mohit <sopranosmohit@gmail.com>
2019-09-17 22:02:02 +0530

    Initial commit

---
+++ src/main/java/io/mylearning/movieinfoservice/MovieResource.java
@@ -0,0 +1,20 @@
+package io.mylearning.movieinfoservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+public Movie getMovieInfo(@PathVariable String movieId)
+{
+    return new Movie("123","Transformer");
+
+
+}
+
+
+}
